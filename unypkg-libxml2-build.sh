fe49ba3879d3c2fef0d54ac914176e9a5cc96b15
michacassola <mich@casso.la>
2024-05-21 18:13:29 +0200

    Update unypkg-libxml2-build.sh

--- unypkg-libxml2-build.sh
+++ unypkg-libxml2-build.sh
@@ -56,7 +56,8 @@ archiving_source
 # shellcheck disable=SC2154
 unyc <<"UNYEOF"
 set -vx
-source /uny/build/functions
+source /uny/git/unypkg/fn
+
 pkgname="libxml2"
 
 version_verbose_log_clean_unpack_cd
